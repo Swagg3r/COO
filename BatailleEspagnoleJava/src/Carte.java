e363eaf75b2b5afab9704a6144755ee57f37992b
thebauda <thebauda@f203-11.e-im2ag.ujf-grenoble.fr>
2015-11-27 17:27:48 +0100

    Javadoc pour Carte

--- BatailleEspagnoleJava/src/Carte.java
+++ BatailleEspagnoleJava/src/Carte.java
@@ -1,34 +1,56 @@
-public class Carte implements Comparable<Carte>{
 
+/**
+ * <b>Carte est la classe qui représente une carte. </b>
+ * <p>Une carte est définie par :</p>
+ * <ul>
+ * <li>son type (2, 3, 4.. As).</li>
+ * <li>sa famille (Bâton, Coupe, Or ou Epée).</li>
+ * </ul>
+ * <p>Cette classe implémente l'interface Comparable.</p>
+ * 
+ * @see famille
+ * @see typeCarte
+ * 
+ * @author Aurélien MONNET-PAQUET, Antoine THEBAUD
+ */
+
+public class Carte implements Comparable<Carte> {
+
+    /**
+     * Le type de la carte (2, 3, 4.. As).
+     * @see Carte#setType()
+     * @see Carte#getType()
+     */
     private typeCarte type;
+    
+    /**
+     * La famille de la carte (Bâton, Coupe, Or ou Epée).
+     * @see Carte#setFamille()
+     * @see Carte#getFamille()
+     */
     private famille nomFamille;
 
-    /* Constructeur */
-    
+    /**
+     * <p>Constructeur de Carte. </p>
+     * A la construction d'un objet Carte, le type et la famille sont initialisés avec les valeurs passées en paramètres.
+     * @param type le type de la carte.
+     * @param nomFamille la famille de la carte.
+     * @see Carte#nomFamille
+     * @see Carte#type
+     */
     public Carte(typeCarte type, famille nomFamille) {
         this.type = type;
         this.nomFamille = nomFamille;
     }
-
-    /* get / set / add / remove */
     
-    // type
-    public typeCarte getType() {
-        return this.type;
-    }
-    public void setType(typeCarte type) {
-        this.type = type;
-    }
-    // nomFamille
-    public famille getFamille() {
-        return this.nomFamille;
-    }
-    public void setFamille(famille nomFamille) {
-        this.nomFamille = nomFamille;
-    }
-    
-    /* Fonctions */
-    
+    /**
+     * <p>Compare la carte avec une autre carte. </p>
+     * Cette fonction est une implémentation de la fonction compareTo() de l'interface Comparable.
+     * La comparaison se fait sur le type de la carte.
+     * @param other la carte à confronter.
+     * @return -1 si l'autre carte est plus forte, 1 si elle est moins forte, 0 si elle est de puissance égale
+     * @see typeCarte
+     */
     @Override
     public int compareTo(Carte other){
         if (this.type.getRank() > other.type.getRank()) {
@@ -40,11 +62,53 @@ public class Carte implements Comparable<Carte>{
         }
     }
     
+    /**
+     * Affiche la carte (son type et sa famille).
+     */
     public void print() {
         System.out.println(this.type+" de "+this.nomFamille);
     }
-    
+    /**
+     * Retourne les informations sur la carte sous forme de chaine de caractère.
+     * @return les informations sur la carte (String)
+     */
     public String toString() {
         return this.type+" de "+this.nomFamille;
+    }
+    
+    /**
+     * Retourne le type de la carte.
+     * @return le type de la carte.
+     * @see Carte#type
+     */
+    public typeCarte getType() {
+        return this.type;
+    }
+    
+    /**
+     * Met à jour le type de la carte.
+     * @param type le nouveau type.
+     * @see Carte#type
+     */
+    public void setType(typeCarte type) {
+        this.type = type;
+    }
+    
+    /**
+     * Retourne la famille de la carte.
+     * @return la famille de la carte.
+     * @see Carte#nomFamille
+     */
+    public famille getFamille() {
+        return this.nomFamille;
+    }
+    
+    /**
+     * Met à jour la famille de la carte.
+     * @param nomFamille la nouvelle famille.
+     * @see Carte#nomFamille
+     */
+    public void setFamille(famille nomFamille) {
+        this.nomFamille = nomFamille;
     }
 }
